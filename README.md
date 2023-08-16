e89ebdaf2f505ddfbfab88c33f90f78926c38265
mflanders06 <mflanders06@gmail.com>
2023-08-16 14:18:59 -0600

    58 minutes in, setup basic state and props

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # React + TypeScript + Vite
 
+Following tutorial: https://www.youtube.com/watch?v=SqcY0GlETPk
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["New York", "San Fancisco", "Tokyo"];
+interface ListGroupProps {
+  heading: string;
+  items: string[];
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -19,6 +24,7 @@ function ListGroup() {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
